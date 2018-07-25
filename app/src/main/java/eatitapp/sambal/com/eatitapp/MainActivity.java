0ded11035b585332d0cbc26c054932ab94fa77e1
Sambal Sriva <sambalsriva>
2018-07-25 18:31:20 +0530

    25/07 commit

--- app/src/main/java/eatitapp/sambal/com/eatitapp/MainActivity.java
+++ app/src/main/java/eatitapp/sambal/com/eatitapp/MainActivity.java
@@ -1,14 +1,40 @@
 package eatitapp.sambal.com.eatitapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button SignIn, SignUp;
+    TextView txtSlogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SignIn =(Button) findViewById(R.id.btnSignIn);
+        SignUp =(Button) findViewById(R.id.btnSignUp);
+
+        txtSlogan = findViewById(R.id.txtSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/NABILA.TTF");
+        txtSlogan.setTypeface(face);
+
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
         //this is new on 23/07/18
